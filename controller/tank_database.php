b0b1dacb364bf60dd0253c21493b87c65dd24c34
Apurboipe <apurbo.ipe@gmail.com>
2018-06-21 17:53:04 +0600

    Dashboard Release version 1.5.0.June-21-2018

---
+++ controller/tank_database.php
@@ -0,0 +1,81 @@
+<?php
+class Tank_database {
+    private $db_connect;
+    public function __construct() {
+        $host_name = 'localhost';
+        $user_name = 'root';
+        $password = '';
+        $db_name = 'datasoft_water_tank';
+        
+        $this->db_connect=  mysqli_connect($host_name, $user_name, $password, $db_name);
+        if(!$this->db_connect){
+            die('Connection Failed'.mysqli_error($this->db_connect));
+        }
+    }
+
+
+    public function select_all_tank_info(){
+        $sql="SELECT * From tank_status";
+          if (mysqli_query($this->db_connect, $sql)){
+          $query_result = mysqli_query($this->db_connect, $sql);
+              return $query_result;
+          } else {
+              die('Query problem' . mysqli_error($this->db_connect));
+          }  
+      }
+
+
+      public function select_all_notifications(){
+        $sql="SELECT * From tank_notification ORDER BY notification_id DESC";
+          if (mysqli_query($this->db_connect, $sql)){
+          $query_result = mysqli_query($this->db_connect, $sql);
+              return $query_result;
+          } else {
+              die('Query problem' . mysqli_error($this->db_connect));
+          }  
+      }
+
+
+      public function notification_details_by_id($notification_id){
+        $sql="SELECT * FROM  tank_notification WHERE notification_id = '$notification_id'";
+          if (mysqli_query($this->db_connect, $sql)){
+          $query_result = mysqli_query($this->db_connect, $sql);
+              return $query_result;
+          } else {
+              die('Query problem' . mysqli_error($this->db_connect));
+          }
+
+        }
+
+      public function update_notification_seen($notification_id){
+          $sql = "UPDATE tank_notification SET seen=1 where notification_id = '$notification_id'"; 
+          if(mysqli_query($this->db_connect, $sql)){
+            return 0;
+          }  else {
+              die('Query problem' . mysqli_error($this->db_connect));
+          }
+        }
+
+      public function select_dropdown_notifications(){
+        $sql="SELECT * From tank_notification where seen = 0 OR danger = 1";
+          if (mysqli_query($this->db_connect, $sql)){
+          $query_result = mysqli_query($this->db_connect, $sql);
+              return $query_result;
+          } else {
+              die('Query problem' . mysqli_error($this->db_connect));
+          }  
+      }    
+
+      
+
+      public function select_all_tank_info_by_id($device_id){
+        $sql="SELECT * From tank_status WHERE device_id = '$device_id'";
+          if (mysqli_query($this->db_connect, $sql)){
+          $query_result = mysqli_query($this->db_connect, $sql);
+              return $query_result;
+          } else {
+              die('Query problem' . mysqli_error($this->db_connect));
+          }  
+      }
+
+}
